399d6e89d9d8dda8416d7e41c70eccfba53ecf75
SEAN PHILLIPS <sldprt@hotmail.com>
2019-10-12 08:04:05 -0700

    calibrationcube

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -729,7 +729,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.0395, 80.0395, 400.48, 99.1 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.5, 78.5, 400.48, 99.1 }
 
 /**
  * Default Max Feed Rate (mm/s)
